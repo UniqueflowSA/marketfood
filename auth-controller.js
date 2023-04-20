e32ad25905e37c60bcdc20ea27100d0f3dcfae2f
김승환 <prpn97@gmail.com>
2023-04-20 15:17:19 +0900

    auth-router/controller/service 구분수정

--- auth-controller.js
+++ auth-controller.js
@@ -1,49 +1,23 @@
-import jwt from "jsonwebtoken";
-import bcrypt from "bcryptjs";
-import { userService } from "../services/user-service";
+import { authService } from "../services/auth-service";
 
-const login = async (req, res, next) => {
+const login = async (req, res) => {
+  const { userId, password } = req.body;
   try {
-    // 클라이언트에서 전달받은 아이디/패스워드 추출
-    const { userId, password } = req.body;
-
-    // 유효한 유저인지 확인
-    const user = await userService.getUserByUsername(userId);
-    if (!user) {
-      return res.status(401).json({ message: "아이디 또는 비밀번호를 잘못 입력했습니다. 입력하신 내용을 다시 확인해주세요." });
-    }
-
-    // 패스워드 검증
-    const passwordMatches = await bcrypt.compare(password, user.password);
-    if (!passwordMatches) {
-      return res.status(401).json({ message: "아이디 또는 비밀번호를 잘못 입력했습니다. 입력하신 내용을 다시 확인해주세요." });
-    }
-
-    // 로그인 성공 시, JWT 토큰 발급
-    const token = jwt.sign(
-      { userId: user.id },
-      process.env.JWT_SECRET,
-      { expiresIn: "1h" } // 토큰 만료 시간: 1시간
-    );
-
+    const token = await authService.login(userId, password);
     res.status(200).json({ token });
-  } catch (error) {
-    next(error);
+  } catch (err) {
+    console.error(err);
+    res.status(401).json({ error: err.message });
   }
 };
-  
-const logout = async (req, res, next) => {
+
+const logout = async (req, res) => {
   try {
-    // 클라이언트에서 전달받은 JWT 토큰 추출
-    const token = req.headers.authorization.split(" ")[1];
-
-    // JWT 토큰 검증
-    await jwt.verify(token, process.env.JWT_SECRET);
-
-    // 로그아웃 성공 시, 응답 반환
-    res.status(200).json({ message: "로그아웃되었습니다." });
-  } catch (error) {
-    next(error);
+    await authService.logout(req.user.id);
+    res.status(204).send();
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "로그아웃에 실패했습니다." });
   }
 };
 
---
+++ auth-service.js
@@ -0,0 +1,24 @@
+import db from "../models";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const login = async (userId, password) => {
+  const user = await db.User.findOne({ where: { userId } });
+  if (!user) {
+    throw new Error("없는 아이디입니다.");
+  }
+  const match = await bcrypt.compare(password, user.password);
+  if (!match) {
+    throw new Error("Invalid login credentials.");
+  }
+  const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
+    expiresIn: "1h",
+  });
+  return token;
+};
+
+const logout = async (userId) => {
+  // implement logout logic, e.g. delete refresh token from DB
+};
+
+export const authService = { login, logout };
