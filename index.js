159551657c054cdaa16ebac6179d020a52df5683
MoonKim <design20458@gmail.com>
2023-04-20 13:39:26 +0900

    0420 백엔드상태포함 첫 커밋

---
+++ index.js
@@ -0,0 +1,8 @@
+import { app } from './src/app';
+import 'dotenv/config';
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+    console.log(`정상적으로 서버를 시작하였습니다.  http://localhost:${PORT}`);
+});
