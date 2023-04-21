295607d2aa0b86019a1912f88e12d0588e3630c2
uniqueflowSA <saco2020a@gmail.com>
2023-04-21 18:27:27 +0900

    cart...buliding_june

--- src/views/cart/cart_srcript.js
+++ src/views/cart/cart_srcript.js
@@ -35,6 +35,43 @@ for(const i of product){
 		const nameDiv = document.createElement('div')
 		const productName = document.createElement('p')
 		productName.textContent = product.name
+		const deleteBtn = document.createElement("button")
+		deleteBtn.classList.add('delete__btn')
+		deleteBtn.textContent = 'Delete'
+		nameDiv.appendChild(productName)
+		nameDiv.appendChild(deleteBtn)
+		productItem.appendChild(nameWrapper)
+
+		const infoDiv = document.createElement('div')
+		const quanDiv = document.createElement('div')
+		const quanLabel = document.createElement('div')
+		quanLabel.textContent = '수량:'
+		const quanMinusBtn = document.createElement('button')
+		quanMinusBtn.classList.add('info__quan__minus__btn')
+		quanMinusBtn.textContent = '-'
+		const quanNum = document.createElement('span')
+		quantity.textContent = product.quantity 
+		const quanPlusBtn = document.createElement('button')
+		quanPlusBtn.classList.add('info__quan__plus__btn')
+		quanPlusBtn.textContent = "+"
+		quanDiv.appendChild(quanLabel)
+		quanDiv.appendChild(quanMinusBtn)
+		quanDiv.appendChild(quantity)
+		quanDiv.appendChild(quanPlusBtn)
+		infoDiv.appendChild(quanDiv)
+
+		const priceDiv = document.createElement('div')
+		const priceLabel = document.createElement('div')
+		priceLabel.textContent = '가격: '
+		const price =document.createElement('div')
+		price.textContent = product.price
+		priceDiv.appendChild(priceLabel)
+		priceDiv.appendChild(price)
+		priceDiv.appendChild(priceDiv)
+
+		productItem.appendChild(infoDiv)
+
+		cartList.appendChild(productItem)
 }
 
 
