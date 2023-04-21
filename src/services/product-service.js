930191dab3b43b64b1e228dd4b7e753d56252a6e
Jonguk Lee <looke2930@gmail.com>
2023-04-21 13:14:51 +0900

    product-service.js 부분 수정

--- src/services/product-service.js
+++ src/services/product-service.js
@@ -1,22 +1,46 @@
-import productModel from "../db/models/product-model";
+import { productModel } from "../db";
 
-const createProduct = async(product) => {
-  const createProduct = await productModel.Product.create(product);
-  return createProduct;
+class ProductService {
+  async addProduct(productInfo) {
+    const createdNewProduct = await productModel.create(productInfo);
+    return createdNewProduct;
+  }
+
+  async getProductById(productId) {
+    const product = await productModel.findById(productId);
+    return product;
+  }
+
+  async getProductList(pidArr) {
+    if (!pidArr) {
+      const productList = await productModel.findAll();
+      return productList;
+    } else {
+      const productList = await productModel.findByIds(pidArr);
+      return productList;
+    }
+  }
+
+  async editProduct(productId, toUpdate) {
+    const updatedProduct = await productModel.update({productId, update : toUpdate});
+    return updatedProduct;
+  }
+
+  async removeProduct(productId) {
+    const { removeCount } = await productModel.delete(productId);
+
+    if(removeCount === 0 ){
+      throw new Error(`${productId} 제품 삭제를 실패했습니다.`);
+    }
+    return {result : "success"};
+  }
+
+  async searchProduct(searchBy) {
+    const productList = await productModel.search(searchBy);
+    return productList;
+  }
 }
 
-const getProduct = async(productId) => {
-  const findProduct = await productModel.Product.findOne({productId});
-  return findProduct;
-}
+const productService = new ProductService();
 
-const updateProduct = async() => {
-
-}
-const deleteProduct = async (productId) => {
-  
-}
-
-
-
-export default {createProduct, getProduct, updateProduct, deleteProduct};
+export { productService };
