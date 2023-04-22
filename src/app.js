43d3b16241c9eef060762830a4ebd8abfd308e75
김승환 <prpn97@gmail.com>
2023-04-22 16:44:17 +0900

    mongoDB DB저장경로변경

--- src/app.js
+++ src/app.js
@@ -25,7 +25,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use("/uploads", express.static(path.resolve(__dirname, "uploads")));
 
 // MongoDB 연결
-mongoose.connect("mongodb://localhost/myapp", {
+mongoose.connect("mongodb://localhost/marketDB", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
