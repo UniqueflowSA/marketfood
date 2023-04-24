37ac323675e0a011305a7221b78cfd8ff5a2d590
김승환 <prpn97@gmail.com>
2023-04-24 20:50:15 +0900

    코치님 피드백 반영

--- src/app.js
+++ src/app.js
@@ -4,7 +4,9 @@ import { fileURLToPath } from "url"; // fileURLToPath 함수 import
 import path from "path";
 import passport from "passport";
 import session from "express-session";
+import dotenv from "dotenv";
 
+dotenv.config();
 import {
   userRouter,
   authRouter,
@@ -41,10 +43,16 @@ app.use(passport.session());
 //   useNewUrlParser: true,
 //   useUnifiedTopology: true,
 // });
-mongoose.connect("mongodb+srv://market:market1234@cluster0.nvy1cxv.mongodb.net/test", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+// mongoose.connect("mongodb+srv://market:market1234@cluster0.nvy1cxv.mongodb.net/test", {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
+mongoose.connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  } )
+.then(console.log("connected to mongodb"))
+.catch(console.error());
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
