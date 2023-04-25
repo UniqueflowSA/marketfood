fda939a41375696e7049e73a4289b00aadbd9f2b
uniqueflowSA <saco2020a@gmail.com>
2023-04-25 09:30:21 +0900

    buliding_order_june

--- src/views/cart/cart_srcript.js
+++ src/views/cart/cart_srcript.js
@@ -31,40 +31,40 @@ window.addEventListener('load',()=>{
 		cartHaveProduct.classList.remove('display__none')
 
 		orders.forEach((product) =>{
-			const{ product_id, productName, price, quantity,image} = product
+			const{ _id, productName, price, quantity,image} = product
 			cartProductList.insertAdjacentHTML('beforeend',
 			`
-			<div id='productItem__${product_id}'>
-				<div class="list__img__block">
-					<a href="#none"><img id="product__img__${product_id}" src="${image}" alt="상품이미지파일"></a>
-				</div>
-				<div class="list__name__block">
-					<!-- 상품정보 파트 -->
-					<span id="product__name__${product_id}">${productName}</span>
-					<button class="btn__defalut" id="select__delete__${product_id}"> Delete</button>
-				</div>
-				<div class="list__info__block">
-					<!-- 수량 및 가격 -->
-					<div class="product__quan">
-						<span>수량:</span>
-						<button class="btn__default" id="minus__btn__${product_id}">-</button>
-						<span id="quan__num__${product_id}">${quantity}</span>
-						<button class="btn__default" id="plus__btn__${product_id}">+</button>
+				<div class="cart__product__list" id='productItem__${_id}'>
+					<div class="list__img__block">
+						<a href="#none"><img id="product__img__${_id}" src=${image} alt="상품이미지파일"></a>
 					</div>
-					<div class="product__price">
-						<span>가격</span>
-						<span id="product__price__num__${product_id}">${price}</span>
+					<div class="list__name__block">
+						<!-- 상품정보 파트 -->
+						<span class="product__name__text" id="product__name__${_id}">${productName}</span>
+						<button class="btn__default sel__del" id="select__delete__${_id}"> Delete</button>
+					</div>
+					<div class="list__info__block">
+						<!-- 수량 및 가격 -->
+						<div class="product__quan">
+							<span>수량:</span>
+							<button class="btn__default" id="minus__btn__${_id}">-</button>
+							<span id="quan__num__${_id}">${quantity}</span>
+							<button class="btn__default" id="plus__btn__${_id}">+</button>
+						</div>
+						<div class="product__price">
+							<span>가격</span>
+							<span id="product__price__num__${_id}">${price}</span>
+						</div>
 					</div>
 				</div>
-			</div>
-			`)
+				`)
 		})
-		const plusBtn = document.querySelector(`#plus__btn__${product_id}`)
-		const minusBtn = document.querySelector(`#minus__btn__${product_id}`)
-		const quantityNum= document.querySelector(`#quan__num__${product_id}`)
-		const productPrice = document.querySelector(`#product__price__num__${product_id}`)
-		const deleteBtn = document.querySelector(`#select__delete__${product_id}`)
-		const productItem = document.querySelector(`#productItem__${product_id}`)
+		const plusBtn = document.querySelector(`#plus__btn__${_id}`)
+		const minusBtn = document.querySelector(`#minus__btn__${_id}`)
+		const quantityNum= document.querySelector(`#quan__num__${_id}`)
+		const productPrice = document.querySelector(`#product__price__num__${_id}`)
+		const deleteBtn = document.querySelector(`#select__delete__${_id}`)
+		const productItem = document.querySelector(`#productItem__${_id}`)
 		const orderPrice = document.querySelector('#order__price__num')
 		const totalPrice = document.querySelector('#total__price__num')
 
@@ -79,14 +79,14 @@ window.addEventListener('load',()=>{
 			productPrice.textContent = (price * Number(quantityNum))
 		})
 		deleteBtn.addEventListener('click', ()=>{
-			const key = localStorage.key(product_id)
+			const key = localStorage.key(_id)
 			localStorage.removeItem(key)
 			productItem.remove()
 		})
 		const orderPriceFunc= ()=>{
 			let orderPriceNum = 0;
 			orders.forEach(product =>{
-				const{product_id, price, quantity} = product
+				const{_id, price, quantity} = product
 				const productPriceNum = price*quantity
 				orderPriceNum += productPriceNum
 			})
@@ -114,8 +114,6 @@ window.addEventListener('load',()=>{
 	
 
 
-
-
 shoppingBtn.addEventListener("click",()=>{
-    window.location.href = 'main.html'
+    window.location.href = '../main/main.html'
 })
