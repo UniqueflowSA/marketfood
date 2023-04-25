3b1c589f586e2b7f54d61d32a47c94aa9b7f4028
Jonguk Lee <looke2930@gmail.com>
2023-04-25 17:30:18 +0900

    상품 전체, 개별 get요청 기능 구현

--- src/db/models/product-model.js
+++ src/db/models/product-model.js
@@ -10,9 +10,41 @@ class ProductModel {
   }
 
   async findById(pId) {
-    const product = await Product.findOne({productId: pId });
+    const product = await Product.findOne({ productId: pId });
     return product;
   }
+  async findByIds(pidArr){
+    try {
+      const productList = new Array();
+
+      for (const pid of pidArr) {
+        const product = await Product.findOne({ _id: pid }).populate(
+          "category"
+        );
+        if (product) {
+          productList.push(product);
+        }
+      }
+      return productList;
+    } catch (err) {
+      const error = new Error("ID기반 상품 리스트 검색에 실패하였습니다.");
+      error.statusCode = 400;
+      throw error;
+    }
+
+  }
+  
+  async findAll() {
+    try {
+      const productList = await Product.find({});
+      return productList;
+    } catch (err) {
+      const error = new Error("상품 수정에 실패하였습니다.");
+      error.statusCode = 400;
+      throw error;
+    }
+  }
+
 
   async create(product) {
     const newProduct = await Product.create(product);
--- src/routers/product-router.js
+++ src/routers/product-router.js
@@ -4,9 +4,9 @@ import { productController } from '../controllers/product-controller.js';
 const productRouter = Router();
 
 productRouter.post('/product', productController.createProduct);
-// productRouter.get('/product', productController.getProductList);
+productRouter.get('/product', productController.getProductList);
 // 제품 등록 시 필요한 데이터 => 제품명, 아이디, 가격, , 올린 사람 
-productRouter.get('/product', productController.getProduct);
+productRouter.get('/product/:productId', productController.getProduct);
 productRouter.put('/product/:productId', productController.updateProduct);
 productRouter.delete('/product/:productId', productController.deleteProduct);
 
