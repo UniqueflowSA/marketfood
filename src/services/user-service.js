e01d21113c270e9c3f43d9b6a672aaac2ec11f80
김승환 <prpn97@gmail.com>
2023-04-25 16:02:05 +0900

    'email','password', 'address', 'phone' 만 변경가능

--- src/services/user-service.js
+++ src/services/user-service.js
@@ -16,13 +16,25 @@ return foundUser;
 };
 
 const updateUser = async (userId, updatedInfo) => {
-const userModel = new UserModel();
-const updatedUser = await userModel.update(userId, updatedInfo);
-if (!updatedUser) {
-throw new Error("정보 수정에 실패했습니다.");
-}
-return updatedUser;
+  const userModel = new UserModel();
+  const allowedUpdates = ['email','password', 'address', 'phone']; // 변경 가능한 프로퍼티
+  const updates = {};
+
+  for (const key in updatedInfo) {
+    if (allowedUpdates.includes(key)) {
+      updates[key] = updatedInfo[key];
+    }
+  }
+  
+  const updatedUser = await userModel.update(userId, updates);
+  
+  if (!updatedUser) {
+    throw new Error("정보 수정에 실패했습니다.");
+  }
+  
+  return updatedUser;
 };
+
 
 const deleteUser = async (userId) => {
   const userModel = new UserModel();
