9f045c76f516d7e628044111ee41e5af4536970b
김승환 <prpn97@gmail.com>
2023-04-26 21:45:24 +0900

    admin token 수정

--- src/db/models/user-model.js
+++ src/db/models/user-model.js
@@ -14,8 +14,10 @@ export default class UserModel {
     const user = await User.findOne({ userId });
     return user
   }
-  
-
+  async find() {
+    const users = await User.find();
+    return users;
+  }
   async create(userInfo) {
     const { password, ...rest } = userInfo;
 
