996ae00d122ce101c8be472b806696d7823d956d
[SW4/1반] 김예은 <christinaye0403@gmail.com>
2023-04-26 17:52:33 +0900

    로그인 fetch 수정

--- src/views/join/join.js
+++ src/views/join/join.js
@@ -72,7 +72,9 @@ submitBtn.onclick = () => {
                 "Content-Type": "application/json",
             },
             body: JSON.stringify(req),
-        });
+        })
+        .then((res) => res.json())
+        .then((res) => console.log(res));
     }
 }
 //onchange_id
