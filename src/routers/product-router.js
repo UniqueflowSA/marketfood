cb3540a8191c8b956ccdaa67aec34477fd86f301
Jonguk Lee <looke2930@gmail.com>
2023-04-26 23:36:45 +0900

    상품 라우터 파일 수정

--- src/routers/product-router.js
+++ src/routers/product-router.js
@@ -5,7 +5,6 @@ const productRouter = Router();
 
 productRouter.post('/product', productController.createProduct);
 productRouter.get('/product', productController.getProductList);
-// 제품 등록 시 필요한 데이터 => 제품명, 아이디, 가격, , 올린 사람 
 productRouter.get('/product/:productId', productController.getProduct);
 productRouter.put('/product/:productId', productController.updateProduct);
 productRouter.delete('/product/:productId', productController.deleteProduct);
