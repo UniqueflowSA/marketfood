852c4f0a597729780699538c5a39d652bdc7d85b
uniqueflowSA <saco2020a@gmail.com>
2023-04-28 00:55:40 +0900

    결제페이지진행

--- src/views/order/order_script.js
+++ src/views/order/order_script.js
@@ -1,79 +1,74 @@
 
+// import { main } from "/public/js/main.js";
+// const {loggedInUser} = await main();
 
-function insertProductsfromOde (){
+const cartDetail = document.querySelector('.cart__detail') //배송비 및 토탈가격 포함 section
+	const cartHaveProduct = document.querySelector('#cart__have__product') //장바구니 상품리스트 페이지
 
-	const localLength = localStorage.length;
-	const orders = []
-	for(let i=1;i<localLength+1;i++){
-		if(localStorage.getItem(String(i))){ //LocalStorage에서 정보가져오기
-			orders.push(JSON.parse(localStorage.getItem(String(i))))
-		}
-	}
+	const dataCount = localStorage.length;
+	let nowcart = [];
+      for(let i = 0; i < dataCount; i++){
+         if(window.localStorage.key(i).includes("cart")){
+					const key = localStorage.key(i);
+					const data = JSON.parse(localStorage.getItem(key))
+          nowcart.push(data)
+         }
+				}
+      
+console.log(nowcart);
 
-	const cartProductList = document.querySelector('.cart__detail')
+		nowcart.forEach((cartItem) =>{
+			const{ _id, product, price, amount, imgUrl} = cartItem
+			cartHaveProduct.insertAdjacentHTML('beforeend', 
+				`<div class="cart__product__list" id='productItem__${_id}'>
+			
+				<div class="list__img__block">
+							<a href="#none"><img id="product__img__${_id}" src=${imgUrl} alt="상품이미지파일"></a>
+						</div>
+						<div class="list__name__block">
+							<!-- 상품정보 파트 -->
+							<span class="product__name__text" id="product__name__${_id}">${product}</span>
+						
+						</div>
+						<div class="list__info__block">
+							<!-- 수량 및 가격 -->
+							<div class="product__quan">
+								<span>수량:</span>
+
+								<input class = "quan__input "id="quan__num__${_id}"value = '${amount}' readonly/>
+							</div>
+							<div class="product__price">
+								<span>가격:</span>
+								<span id="product__price__num__${_id}">${price*amount}</span> 
+							</div>
+						</div>
+					</div>
+					`)
+		})
+			const amountNum= document.querySelector(`#quan__num__${_id}`)
+			const productPrice = document.querySelector(`#product__price__num__${_id}`)
+			const productItem = document.querySelector(`#productItem__${_id}`)
+			const orderPrice = document.querySelector('#order__price__num')
+			const totalPrice = document.querySelector('#total__price__num')
+
+			const orderPriceFunc= ()=>{
+					let orderPriceNum = 0;
+						nowcart.forEach((cartItem) =>{
+							const{ _id, price, amount} = cartItem
+						let productPriceNum = price*amount  
+						orderPriceNum += productPriceNum
+					})
+					orderPrice.textContent = orderPriceNum
+					if(parseInt(orderPrice.textContent)>50000){
+						totalPrice.textContent = orderPrice.textContent 
+				} else if(parseInt(orderPrice.textContent)<50000){
+					totalPrice.textContent =  parseInt(orderPrice.textContent)+2500
+				}
+			}
 	
-		orders.forEach((product) =>{
-			const{ _id, productName, price, quantity,image} = product
-			cartProductList.insertAdjacentHTML('beforeend',
-			`
-			<div class="cart__product__list" id='productItem__${_id}'>
-      <div class="list__img__block">
-        <a href="#none"><img id="product__img__${_id}" src="${image}" alt="상품이미지파일"></a>
-      </div>
-      <div class="list__name__block">
-        <!-- 상품정보 파트 -->
-        <span class="product__name__text" id="product__name__${_id}">${productName}</span>
-      </div>
-      <div class="list__info__block">
-        <!-- 수량 및 가격 -->
-        <div class="product__quan">
-          <span>수량:</span>
-          <button class="btn__default minusbtn" id="minus__btn__${_id}">-</button>
-          <span id="quan__num__${_id}">${quantity}</span>
-          <button class="btn__default plusbtn" id="plus__btn__${_id}">+</button>
-        </div>
-        <div class="product__price">
-          <span>가격:</span>
-          <span id="product__price__num__${_id}">${price}</span>
-        </div>
-      </div>
-    </div>
-				`)
-		})
-		const plusBtn = document.querySelector(`#plus__btn__${_id}`)
-		const minusBtn = document.querySelector(`#minus__btn__${_id}`)
-		const quantityNum= document.querySelector(`#quan__num__${_id}`)
-		const productPrice = document.querySelector(`#product__price__num__${_id}`)
-		const productItem = document.querySelector(`#productItem__${_id}`)
-		const orderPrice = document.querySelector('#order__price__num')
-		const totalPrice = document.querySelector('#total__price__num')
-		const orderBtn = document.querySelector("#order__btn")
-	
-
-
-		let crrentNum = parseInt(quantityNum.textContent)
-		plusBtn.addEventListener('click',()=>{
-			quantityNum.textContent = crrentNum + 1
-			productPrice.textContent = (price * Number(quantityNum))
-		})
-		minusBtn.addEventListener('click',()=>{
-			quantityNum.textContent = crrentNum - 1
-			productPrice.textContent = (price * Number(quantityNum))
-		})
-		const orderPriceFunc= ()=>{
-			let orderPriceNum = 0;
-			orders.forEach(product =>{
-				const{_id, price, quantity} = product
-				const productPriceNum = price*quantity
-				orderPriceNum += productPriceNum
-			})
-			orderPrice.textContent = orderPriceNum
-			totalPrice.textContent = (orderPriceNum*1)+2500  
-		}
-		orderPriceFunc()
 
 		orderBtn.addEventListener('click',()=>{ // 주문자페이지로 데이터보내기
 			
 		})
-}
+		orderPriceFunc()
 	
