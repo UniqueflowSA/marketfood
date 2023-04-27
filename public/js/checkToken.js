6ce60c1b245bb2dd8fc42e2f7e77ae0c67729f3a
MoonKim <design20458@gmail.com>
2023-04-27 12:56:29 +0900

    토큰 인지 방식 변경요망

--- public/js/checkToken.js
+++ public/js/checkToken.js
@@ -1,19 +1,20 @@
 // 백엔드의 checkToken api를 통해 토큰 확인
 
-function checkToken() { //토큰체크 api 작성 완료 안됨
-    if(document.cookie.includes("jwt_token")){
-        const checkResult = fetch("http://localhost:4000/auth/checkToken")
-            .then(res => res.json())
-            .then(result => {
-                const loggedInUser = result;
-                return {loggedInUser}
-            })
-            .catch((e)=>{
-                alert(e);
-            });
+// function checkToken() { //토큰체크 api 작성 완료 안됨
+//     if(document.cookie.includes("jwt_token")){
+//         const checkResult = fetch("http://localhost:4000/auth/checkToken")
+//             .then(res => res.json())
+//             .then(result => {
+//                 const loggedInUser = result;
+//                 return {loggedInUser}
+//             })
+//             .catch((e)=>{
+//                 alert(e);
+//             });
         
-        return checkResult;
-    }
-}
+//         return checkResult;
+//     }
+// }
+
 
 export { checkToken } ;
