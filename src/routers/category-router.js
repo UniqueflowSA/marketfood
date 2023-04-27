d64cd25929b523a32543e610206afa489bb81e0d
Jonguk Lee <looke2930@gmail.com>
2023-04-27 18:04:46 +0900

    각 라우터 로그인, 관리자 확인 미들웨어 추가

--- src/routers/category-router.js
+++ src/routers/category-router.js
@@ -1,15 +1,17 @@
 import { Router } from "express";
+import { adminOnly } from "../middlewares/index.js"
+import { loginRequired } from "../middlewares/index.js"
 
 import { categoryController } from "../controllers/category-controller.js";
 // import { bodyEmptyChecker } from "../middlewares";
 
 const categoryRouter = Router();
 
-categoryRouter.post("/category", categoryController.createCategory);
+categoryRouter.post("/category",loginRequired, adminOnly, categoryController.createCategory);
 categoryRouter.get("/category", categoryController.getCategoryList);
 // categoryRouter.get("category/search", categoryController.getCategoryByName);
-categoryRouter.get("/category/:categoryId", categoryController.getCategoryById);
-categoryRouter.put("/category/:categoryId", categoryController.updateCategory);
-categoryRouter.delete("/category/:categoryId", categoryController.deleteCategory);
+categoryRouter.get("/category/:categoryId",loginRequired, adminOnly, categoryController.getCategoryById);
+categoryRouter.put("/category/:categoryId", loginRequired, adminOnly,categoryController.updateCategory);
+categoryRouter.delete("/category/:categoryId",loginRequired, adminOnly, categoryController.deleteCategory);
 
 export default categoryRouter;
