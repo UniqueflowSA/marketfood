10e8365df2db668051982558a263cb691e25daf5
uniqueflowSA <saco2020a@gmail.com>
2023-04-27 14:42:11 +0900

    fix_cart

--- src/views/cart/cart_srcript.js
+++ src/views/cart/cart_srcript.js
@@ -15,14 +15,14 @@ const shoppingBtn = document.querySelector(".shopping__page__btn")
 
 const btn = document.querySelector('.btn')
 let productObj ={
-  _id : 32432432, 
-  productName: 'chill', 
+  _id : 32432432222, 
+  product: 'chill', 
   price: 40000, 
-  quantity: 2,
-  image: './image/art-4919768.gif',
+  amount: 2,
+  imgUrl: './image/art-4919768.gif',
 }
 
-btn.addEventListener('click',()=>{
+btn.addEventListener('click',()=>{ 
   localStorage.setItem(Math.floor(Math.random()*100), JSON.stringify(productObj))
 
   console.log('이게뭐지')
@@ -49,17 +49,17 @@ if(dataCount > 0){//장바구니 상품이 있을 때
 		cartHaveProduct.classList.remove('display__none')
 		cartDetail.classList.remove('display__none')
 
-		orders.forEach((product) =>{
-			const{ _id, productName, price, quantity,image} = product
+		orders.forEach((cartItem) =>{
+			const{ _id, product, price, amount, imgUrl} = cartItem
 			cartHaveProduct.insertAdjacentHTML('beforeend', 
 				`<div class="cart__product__list" id='productItem__${_id}'>
 			
 				<div class="list__img__block">
-							<a href="#none"><img id="product__img__${_id}" src=${image} alt="상품이미지파일"></a>
+							<a href="#none"><img id="product__img__${_id}" src=${imgUrl} alt="상품이미지파일"></a>
 						</div>
 						<div class="list__name__block">
 							<!-- 상품정보 파트 -->
-							<span class="product__name__text" id="product__name__${_id}">${productName}</span>
+							<span class="product__name__text" id="product__name__${_id}">${product}</span>
 							<button class="btn__default sel__del" id="select__delete__${_id}"> Delete</button>
 						</div>
 						<div class="list__info__block">
@@ -67,12 +67,12 @@ if(dataCount > 0){//장바구니 상품이 있을 때
 							<div class="product__quan">
 								<span>수량:</span>
 								<button class="btn__default" id="minus__btn__${_id}">-</button>
-								<span id="quan__num__${_id}">${quantity}</span>
+								<input id="quan__num__${_id}"value = '${amount}'>
 								<button class="btn__default" id="plus__btn__${_id}">+</button>
 							</div>
 							<div class="product__price">
 								<span>가격:</span>
-								<span id="product__price__num__${_id}">${price*quantity}</span>
+								<span id="product__price__num__${_id}">${price*amount}</span> 
 							</div>
 						</div>
 					</div>
@@ -80,24 +80,27 @@ if(dataCount > 0){//장바구니 상품이 있을 때
 
 			const plusBtn = document.querySelector(`#plus__btn__${_id}`)
 			const minusBtn = document.querySelector(`#minus__btn__${_id}`)
-			const quantityNum= document.querySelector(`#quan__num__${_id}`)
+			const amountNum= document.querySelector(`#quan__num__${_id}`)
 			const productPrice = document.querySelector(`#product__price__num__${_id}`)
 			const deleteBtn = document.querySelector(`#select__delete__${_id}`)
 			const productItem = document.querySelector(`#productItem__${_id}`)
-			const orderPrice = document.querySelector('#order__price__num')
-			const totalPrice = document.querySelector('#total__price__num')
-			const orderpageBtn = document.querySelector('.order__page__btn') 
+			 
 
-			let currentNum = parseInt(quantityNum.textContent)
+			
 
-			//제품수량변경버튼 수정필요
 			plusBtn.addEventListener('click',()=>{
-				quantityNum.textContent = currentNum + 1
-				productPrice.textContent = (Number(price) * Number(quantityNum.textContent))
+				 amountNum.value = Number(amountNum.value)+1
+				 productPrice.innerHTML = (price * Number(amountNum.value))
 			})
 			minusBtn.addEventListener('click',()=>{
-				quantityNum.textContent = currentNum - 1
-				productPrice.textContent = (Number(price) * Number(quantityNum.textContent))
+				if(Number(amountNum.value) > 1){
+					amountNum.value = Number(amountNum.value)-1
+					productPrice.innerHTML = (price * Number(amountNum.value))
+				} else if(Number(amountNum.value) <= 1){
+					alert("수량 최소값입니다.")
+				}
+				
+
 			})
 			deleteBtn.addEventListener('click', ()=>{
 				const key = localStorage.key(_id)
@@ -105,12 +108,15 @@ if(dataCount > 0){//장바구니 상품이 있을 때
 				productItem.remove()
 				location.reload()
 			})
+			const orderPrice = document.querySelector('#order__price__num')
+			const totalPrice = document.querySelector('#total__price__num')
+			const orderpageBtn = document.querySelector('.order__page__btn')
 
 			const orderPriceFunc= ()=>{
 					let orderPriceNum = 0;
-					orders.forEach(product =>{
-						const{_id, price, quantity} = product
-						const productPriceNum = price*quantity
+					orders.forEach(cartItme =>{
+						const{_id, price, amount} = cartItem
+						const productPriceNum = price*amount
 						orderPriceNum += productPriceNum
 					})
 					orderPrice.textContent = orderPriceNum
@@ -141,85 +147,4 @@ if(dataCount > 0){//장바구니 상품이 있을 때
 		}
 		
 	})
-
-
-
- 
-
-
-
-// function insertProductsfromCart(){
-	// const localLength = localStorage.length;
-	// const orders = []
-
-	// for(let i=0;i<localLength;i++){
-	// 	if(localStorage.getItem(String(i))){ //LocalStorage에서 정보가져오기
-	// 		orders.push(JSON.parse(localStorage.getItem(String(i))))
-	// 	}
-	// }
-
-	
-// 	const cartEmpty = document.querySelector('#cart__empty')
-// 	const cartHaveProduct = document.querySelector('#cart__have__product')
-// 	const allDeleteBtn = document.querySelector('#all__delete')
-	
-// 			const plusBtn = document.querySelector(`#plus__btn__${_id}`)
-// 			const minusBtn = document.querySelector(`#minus__btn__${_id}`)
-// 			const quantityNum= document.querySelector(`#quan__num__${_id}`)
-// 			const productPrice = document.querySelector(`#product__price__num__${_id}`)
-// 			const deleteBtn = document.querySelector(`#select__delete__${_id}`)
-// 			const productItem = document.querySelector(`#productItem__${_id}`)
-// 			const orderPrice = document.querySelector('#order__price__num')
-// 			const totalPrice = document.querySelector('#total__price__num')
-// 			const orderpageBtn = document.querySelector('.order__page__btn') 
-			
-
-
-// 			let currentNum = parseInt(quantityNum.textContent)
-
-// 			plusBtn.addEventListener('click',()=>{
-// 				quantityNum.textContent = currentNum + 1
-// 				productPrice.textContent = (price * Number(quantityNum))
-// 			})
-// 			minusBtn.addEventListener('click',()=>{
-// 				quantityNum.textContent = currentNum - 1
-// 				productPrice.textContent = (price * Number(quantityNum))
-// 			})
-// 			deleteBtn.addEventListener('click', ()=>{
-// 				const key = localStorage.key(_id)
-// 				localStorage.removeItem(key)
-// 				productItem.remove()
-// 			})
-// 			allDeleteBtn.addEventListener('click',()=> {
-// 				window.localStorage.clear();
-// 			})
-// 			const orderPriceFunc= ()=>{
-// 				let orderPriceNum = 0;
-// 				orders.forEach(product =>{
-// 					const{_id, price, quantity} = product
-// 					const productPriceNum = price*quantity
-// 					orderPriceNum += productPriceNum
-// 				})
-// 				orderPrice.textContent = orderPriceNum
-// 				totalPrice.textContent = (orderPriceNum*1)+2500  
-// 			}
-// 			orderPriceFunc()
-			
-// 			orderpageBtn.addEventListener('click',()=>{
-// 				window.location.href = '../order/index.html'
-// 			})
-// 		})
-
-// 	}else if(orders.length === 0 ){ //장바구니 비었을 때
-
-// 		allDeleteBtn.classList.add('display__none')
-// 		cartEmpty.classList.remove('display__none')
-// 		cartHaveProduct.classList.add('display__none')
-// 	}
-// }
-// shoppingBtn.addEventListener("click",()=>{
-// 	window.location.href = '../main/main.html'
-// })
-
-// insertProductsfromCart()
 
