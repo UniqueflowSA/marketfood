0aaa14d3b540dec4097e702129c1101a2f475e3c
uniqueflowSA <saco2020a@gmail.com>
2023-04-29 12:07:04 +0900

    퍼블릭푸터 팀명변경 임준영

--- src/views/public/js/footerRender.js
+++ src/views/public/js/footerRender.js
@@ -4,8 +4,8 @@ function footerRander() {
     footer.innerHTML = `
     <div class="footer">
         <div class="footer-container">
-            <p>2023 Team 부글부글 - 김문진 / 김승환 / 김예은 / 이수영 / 이종욱 / 임준영</p>
-            <p>Copyright ⓒ 2023 - 부글부글. All rights reserved</p>
+            <p>2023 Team 삼고초려 - 김문진 / 김승환 / 김예은 / 이수영 / 이종욱 / 임준영</p>
+            <p>Copyright ⓒ 2023 - 삼고초려. All rights reserved</p>
         </div>
     </div>`;
   }
